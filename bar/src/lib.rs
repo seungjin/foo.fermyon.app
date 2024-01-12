62f5d332f64e475a3bede6736f2def7138d2e7c5
Seungjin Kim <seungjin@duck.com>
2024-01-12 17:18:53 +0900

    check foo_key

--- bar/src/lib.rs
+++ bar/src/lib.rs
@@ -10,6 +10,9 @@ async fn handle_route(req: Request) -> Response {
         println!("{key}: {value}");
     }
 
+    let foo_key = std::env::var("FOO_KEY").unwrap();
+    println!("FOO_KEY--->{foo_key}");
+
     let mut router = Router::new();
     router.get("/bar", bar);
     router.handle(req)
