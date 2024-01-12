6dc2f990d87afa7307011dbc0fa4ff95eca00774
Seungjin Kim <seungjin@duck.com>
2024-01-12 17:15:03 +0900

    env variable not working

--- bar/src/lib.rs
+++ bar/src/lib.rs
@@ -1,51 +1,20 @@
 use spin_sdk::{
-    http::{
-        send, IncomingResponse, IntoResponse, Json, Method, Params, Request, RequestBuilder,
-        Response, Router,
-    },
-    http_component, variables,
+    http::{IntoResponse, Params, Request, Response, Router},
+    http_component,
 };
-
-#[derive(serde::Deserialize, Debug)]
-struct Order {
-    name: String,
-}
 
 #[http_component]
 async fn handle_route(req: Request) -> Response {
+    println!("------");
+    for (key, value) in std::env::vars() {
+        println!("{key}: {value}");
+    }
+
     let mut router = Router::new();
-    router.get("/healthz", health);
-    router.post_async("/q-order-ingress", ingress);
+    router.get("/bar", bar);
     router.handle(req)
 }
 
-fn health(_req: Request, _param: Params) -> anyhow::Result<impl IntoResponse> {
-    Ok(Response::new(200, format!("Healthy")))
-}
-
-async fn ingress(
-    req: http::Request<Json<Order>>,
-    _param: Params,
-) -> anyhow::Result<impl IntoResponse> {
-    let dapr_url = variables::get("dapr_url")?;
-
-    println!("name: {}", req.body().name);
-    println!("dapr_url: {}", dapr_url);
-
-    let request = RequestBuilder::new(Method::Post, dapr_url)
-        .method(Method::Post)
-        .body("xx")
-        .build();
-
-    let response: IncomingResponse = match send(request).await {
-        Ok(response) => response,
-        Err(_) => panic!("error when calling outbound binding"),
-    };
-
-    let status = response.status();
-    Ok(Response::builder()
-        .status(200)
-        .header("content-type", "text/plain")
-        .body(format!("response status: {status}"))
-        .build())
+fn bar(_req: Request, _param: Params) -> anyhow::Result<impl IntoResponse> {
+    Ok(Response::new(200, format!("foo dot bar")))
 }
