8dfde91708802b7038343871943c16f9b96b8628
Seungjin Kim <seungjin@duck.com>
2024-03-09 14:52:45 +0900

    working sample code of getting a file from html form and passing to oracle cloud s3

--- bar/src/lib.rs
+++ bar/src/lib.rs
@@ -2,13 +2,13 @@ use anyhow::bail;
 use anyhow::Result;
 use chrono::{DateTime, Utc};
 use hmac::{Mac, SimpleHmac};
-use multipart::server::Multipart;
+use multipart_2021::server::buf_redux::BufWriter;
+use multipart_2021::server::Multipart;
 use sha2::{Digest, Sha256};
 use spin_sdk::http::{IntoResponse, Method, Request, Response};
 use spin_sdk::http_component;
 use std::fs::File;
 use std::io::prelude::*;
-use std::io::BufRead;
 use std::str;
 use substring::Substring;
 
@@ -32,16 +32,20 @@ async fn handle_bar(req: Request) -> anyhow::Result<impl IntoResponse> {
     let body = req.body();
 
     let l = body.len();
+    println!("body size: {}", l);
 
-    let mut mp = Multipart::with_body(body, boundary);
+    let mut mp: Multipart<&[u8]> = Multipart::with_body(body, boundary);
 
     while let Some(mut field) = mp.read_entry().unwrap() {
-        let data = field.data.fill_buf().unwrap();
-        let s = String::from_utf8_lossy(data);
+        //let s = String::from_utf8_lossy(data);
+        let data: Result<Vec<u8>, std::io::Error> = field.data.bytes().collect();
+        //let size_of_data = data.unwrap().len();
+        let a = data.unwrap();
+        let b: &[u8] = &a;
 
-        //println!("headers: {:?}, data: {}", field.headers, s);
+        //println!("headers: {:?}, data: {}", field.headers, size_of_data);
         match field.headers.filename {
-            Some(x) => send_to_s3(x, data).await?,
+            Some(x) => send_to_s3(x, b).await?,
             None => {}
         }
     }
@@ -57,7 +61,7 @@ pub fn get_multipart_boundary(req: &Request) -> Result<String> {
     let boundary = req.header("content-type").unwrap();
     let a = boundary.as_str().unwrap();
     if a.substring(0, 30) == "multipart/form-data; boundary=" {
-        let mut split = a.split("boundary=").collect::<Vec<&str>>();
+        let split = a.split("boundary=").collect::<Vec<&str>>();
         let a = split[1];
         return Ok(a.to_string());
     }
@@ -87,20 +91,26 @@ curl --location --request PUT 'https://cnbbb4fp6bwv.compat.objectstorage.ap-seou
 // https://docs.aws.amazon.com/AmazonS3/latest/API/sig-v4-header-based-auth.html
 
 pub async fn send_to_s3(file_name: String, file: &[u8]) -> Result<()> {
-    let host = "s3.amazonaws.com";
-    let bucket = "seungjin";
-    let target = format!("https://{bucket}.{host}/{file_name}");
+    //let host = "s3.amazonaws.com";
+    //let bucket = "seungjin";
+    //let target = format!("https://{bucket}.{host}/{file_name}");
+    //let region = "us-east-1".to_string();
+
+    let host = "cnbbb4fp6bwv.compat.objectstorage.ap-seoul-1.oraclecloud.com";
+    let bucket = "tmp";
+    let target = format!("https://{host}/{bucket}/{file_name}");
+    let region = "ap-seoul-1".to_string();
 
     let access_key = std::env::var("S3_ACCESS_KEY").unwrap();
     let secret_key = std::env::var("S3_SECRET_KEY").unwrap();
 
     let content_type = "image/png";
     let content_length = file.len().to_string();
+    println!("--- content length : {}", content_length);
 
-    let region = "us-east-1".to_string();
     let service = "s3".to_string();
 
-    let (x_amz_date, ddate) = get_x_amz_date();
+    let (x_amz_date, ddate) = get_x_amz_date().await;
     let yyyymmdd = x_amz_date.substring(0, 8).to_string();
 
     /*example from AWS doc
@@ -114,14 +124,18 @@ pub async fn send_to_s3(file_name: String, file: &[u8]) -> Result<()> {
     let ddate = "Fri, 24 May 2013 00:00:00 GMT";
     */
 
-    let x_amz_content_sha256 = sha256hash_hex_encoded(file).unwrap();
+    let x_amz_content_sha256 = sha256hash_hex_encoded(file).await.unwrap();
     let xxx = x_amz_content_sha256.clone();
 
     let http_method = "PUT";
     let url_encoded_file_name = urlencoding::encode(file_name.as_str());
-    let canonical_uri = format!("/{url_encoded_file_name}");
+    //let canonical_uri = format!("/{url_encoded_file_name}");
+    let canonical_uri = format!("/{bucket}/{url_encoded_file_name}");
     let canonical_query_string = "";
-    let canonical_headers = format!("date:{ddate}\nhost:{bucket}.{host}:443\nx-amz-content-sha256:{xxx}\nx-amz-date:{x_amz_date}\n");
+    //let canonical_headers = format!("date:{ddate}\nhost:{bucket}.{host}:443\nx-amz-content-sha256:{xxx}\nx-amz-date:{x_amz_date}\n");
+    let canonical_headers = format!(
+        "date:{ddate}\nhost:{host}:443\nx-amz-content-sha256:{xxx}\nx-amz-date:{x_amz_date}\n"
+    );
     let signed_headers = "date;host;x-amz-content-sha256;x-amz-date";
     let hashed_paylod = x_amz_content_sha256.clone();
     let canonical_request = format!(
@@ -130,7 +144,9 @@ pub async fn send_to_s3(file_name: String, file: &[u8]) -> Result<()> {
 
     println!("\n\ncanonical_request:\n{}\n\n", canonical_request);
 
-    let sha256_of_canonical_request = sha256hash_hex_encoded(canonical_request.as_bytes()).unwrap();
+    let sha256_of_canonical_request = sha256hash_hex_encoded(canonical_request.as_bytes())
+        .await
+        .unwrap();
     println!(
         "sha256_of_canonical_request: {}",
         sha256_of_canonical_request,
@@ -148,23 +164,30 @@ pub async fn send_to_s3(file_name: String, file: &[u8]) -> Result<()> {
         format!("AWS4{secret_key}").into_bytes(),
         yyyymmdd.clone().into_bytes(),
     )
+    .await
     .unwrap();
-    let date_region_key = hmac_sha256(date_key, region.clone().into_bytes()).unwrap();
-    let date_region_service_key =
-        hmac_sha256(date_region_key, service.clone().into_bytes()).unwrap();
+    let date_region_key = hmac_sha256(date_key, region.clone().into_bytes())
+        .await
+        .unwrap();
+    let date_region_service_key = hmac_sha256(date_region_key, service.clone().into_bytes())
+        .await
+        .unwrap();
     let signing_key = hmac_sha256(
         date_region_service_key,
         "aws4_request".to_string().into_bytes(),
     )
+    .await
     .unwrap();
 
     let string_to_sign = format!(
         "AWS4-HMAC-SHA256\n{x_amz_date}\n{yyyymmdd}/{region}/{service}/aws4_request\n{sha256_of_canonical_request}"
     );
 
-    let signature = hmac_sha256(signing_key, string_to_sign.clone().into_bytes()).unwrap();
+    let signature = hmac_sha256(signing_key, string_to_sign.clone().into_bytes())
+        .await
+        .unwrap();
 
-    let signature_string = hex_encoded(signature).unwrap();
+    let signature_string = hex_encoded(signature).await.unwrap();
 
     println!("string_to_sign: {string_to_sign}");
     println!("signature: {}", signature_string);
@@ -182,7 +205,7 @@ pub async fn send_to_s3(file_name: String, file: &[u8]) -> Result<()> {
         .uri(target)
         .header("Authorization", authorization)
         .header("Content-Length", content_length)
-        //.header("Content-Type", content_type)
+        //.header("Content-Type", "text/plain")
         .header("X-Amz-Content-Sha256", x_amz_content_sha256)
         .header("X-Amz-Date", x_amz_date)
         .header("Date", ddate)
@@ -200,7 +223,7 @@ pub async fn send_to_s3(file_name: String, file: &[u8]) -> Result<()> {
     Ok(())
 }
 
-pub fn get_x_amz_date() -> (String, String) {
+pub async fn get_x_amz_date() -> (String, String) {
     let current_time: DateTime<Utc> = Utc::now();
     (
         current_time.format("%Y%m%dT%H%M%SZ").to_string(),
@@ -214,7 +237,7 @@ pub fn get_x_amz_date() -> (String, String) {
 // HMAC(key, data) represents an HMAC-SHA256 function that returns output in binary format.
 // The result of each hash function becomes input for the next one.
 // https://stackoverflow.com/questions/67656612/how-to-compute-hmac-sha-256-for-aws-authentication
-pub fn hmac_sha256(key: Vec<u8>, text: Vec<u8>) -> Result<Vec<u8>> {
+pub async fn hmac_sha256(key: Vec<u8>, text: Vec<u8>) -> Result<Vec<u8>> {
     type HmacSha256 = SimpleHmac<Sha256>;
     let mut mac = HmacSha256::new_from_slice(&key).expect("Error from hmac_sha256");
     mac.update(&text);
@@ -223,7 +246,7 @@ pub fn hmac_sha256(key: Vec<u8>, text: Vec<u8>) -> Result<Vec<u8>> {
     Ok(f2)
 }
 
-pub fn sha256hash_hex_encoded(data: &[u8]) -> Result<String> {
+pub async fn sha256hash_hex_encoded(data: &[u8]) -> Result<String> {
     let mut hasher = Sha256::new();
     //hasher.update(data);
     hasher.update(data);
@@ -231,6 +254,6 @@ pub fn sha256hash_hex_encoded(data: &[u8]) -> Result<String> {
     Ok(hex::encode(result))
 }
 
-pub fn hex_encoded(a: Vec<u8>) -> Result<String> {
+pub async fn hex_encoded(a: Vec<u8>) -> Result<String> {
     Ok(hex::encode(a))
 }
