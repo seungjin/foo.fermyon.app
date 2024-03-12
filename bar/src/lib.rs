c6fb905fe06d21d4a3cee3b44ba7a8d6e186445b
Seungjin Kim <seungjin@duck.com>
2024-03-12 12:03:40 +0900

    Dockernize + passing arg test

--- bar/src/lib.rs
+++ bar/src/lib.rs
@@ -2,6 +2,7 @@ use anyhow::Result;
 use spin_sdk::{
     http::{IntoResponse, Request, Response},
     http_component,
+    variables
 };
 use url::Url;
 
@@ -13,6 +14,18 @@ async fn handle_bar(req: Request) -> anyhow::Result<impl IntoResponse> {
     let u = req.uri();
     println!("{u}");
     let parsed_url = Url::parse(u)?;
+
+
+    let (arg_1, arg_2, arg_3, arg_4, arg_5, arg_6) =
+        ( variables::get("arg_foo").expect("Not getting arg1foo"),
+          variables::get("arg2").unwrap(),
+          variables::get("arg3").unwrap(),
+          variables::get("arg4").unwrap(),
+          variables::get("arg5").unwrap(),
+          variables::get("arg6").unwrap()
+    );
+
+    println!("{}, {}, {}, {}, {} ,{}", arg_1, arg_2, arg_3, arg_4, arg_5, arg_6);
 
     Ok(
         Response::builder()
