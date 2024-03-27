c6d30c4d0c4eca54bd430ee2e6de51c5e1bfe725
Seungjin Kim <seungjin@duck.com>
2024-03-27 13:16:30 +0900

    sample code more like sample

--- bar/src/lib.rs
+++ bar/src/lib.rs
@@ -1,18 +1,27 @@
 use anyhow::Result;
 use spin_cron_sdk::{cron_component, Error, Metadata};
 use spin_sdk::http::{Request, Response};
+use spin_sdk::variables;
 
 #[cron_component]
 async fn handle_cron_event(metadata: Metadata) -> Result<(), Error> {
-    let _ = foo().await;
+    let _ = foo(metadata).await;
     Ok(())
 }
 
-async fn foo() -> Result<()> {
-    let url = "https://reqbin.com/echo/get/json";
-    let resp: Response = spin_sdk::http::send(Request::get(url)).await.unwrap();
+async fn foo(metadata: Metadata) -> Result<()> {
+    let key = variables::get("something").unwrap_or_default();
+    let resp: Response = spin_sdk::http::send(Request::get(
+        "https://random-data-api.fermyon.app/animals/json",
+    ))
+    .await
+    .unwrap();
 
-    let a = resp.status();
-    println!("----{}", a);
+    println!("{:#?}", resp);
+
+    println!(
+        "[{}] Hello this is me running every {}",
+        metadata.timestamp, key
+    );
     Ok(())
 }
