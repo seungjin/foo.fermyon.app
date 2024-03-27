28d0c90aefc3c294dd683551a735fe890e0c59ab
Seungjin Kim <seungjin@duck.com>
2024-03-27 14:21:37 +0900

    this not working

--- bar/src/lib.rs
+++ bar/src/lib.rs
@@ -5,7 +5,20 @@ use spin_sdk::variables;
 
 #[cron_component]
 async fn handle_cron_event(metadata: Metadata) -> Result<(), Error> {
-    let _ = foo(metadata).await;
+    //let _ = foo(metadata).await;
+    let key = variables::get("something").unwrap_or_default();
+    let resp: Response = spin_sdk::http::send(Request::get(
+        "https://random-data-api.fermyon.app/animals/json",
+    ))
+    .await
+    .unwrap();
+
+    println!("{:#?}", resp);
+
+    println!(
+        "[{}] Hello this is me running every {}",
+        metadata.timestamp, key
+    );
     Ok(())
 }
 
