2e5faee533058f377f1a2a883900fe029e341b1a
Seungjin Kim <seungjin@duck.com>
2025-04-08 08:14:40 +0700

    I am ablet to http-request without allowed_outbound_hosts

--- apps/cron-me/src/lib.rs
+++ apps/cron-me/src/lib.rs
@@ -1,7 +1,7 @@
 use serde_json::json;
 use serde_json::Value;
 use spin_cron_sdk::{cron_component, Metadata};
-use spin_sdk::http::{ErrorCode, IntoResponse, Method, Request, Response};
+use spin_sdk::http::{ErrorCode, IntoResponse, Method::Get, Request, Response};
 use std::env;
 use std::error::Error;
 use std::str;
@@ -9,6 +9,16 @@ use thiserror::Error;
 
 #[cron_component]
 async fn handle_cron_event(metadata: Metadata) -> anyhow::Result<()> {
-    println!("foo foo");
+    let request = Request::builder()
+        .method(Get)
+        .uri("https://www.fermyon.com/")
+        .build();
+
+    // Send the request and await the response
+    let response: Response = spin_sdk::http::send(request).await?;
+
+    // Use the outbound response body
+    println!("{}", response.status());
+
     Ok(())
 }
